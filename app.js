cfc1380f347cfe64582d01c37ebaefb317976370
sanderdebr <sander606@gmail.com>
2020-04-13 18:35:39 +0200

    update

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ import Config from "./config.js";
 class App {
   constructor() {
     this.tool = "box";
-    this.color = null;
+    this.color = Config.defaultColor;
     this.objects = [];
-    this.width = elements.scene.offsetWidth;
-    this.height = elements.scene.offsetHeight;
     this.grid = { x: [], y: [] };
   }
 
@@ -18,7 +16,7 @@ class App {
     element.classList.add("element");
     element.style.left = this.snapToGrid(e).left;
     element.style.top = this.snapToGrid(e).top;
-    element.style.backgroundColor = "purple";
+    element.style.backgroundColor = this.color;
     this.objects.push(element);
     elements.scene.appendChild(element);
   }
@@ -38,12 +36,13 @@ class App {
   }
 
   // Generates a grid on the scene
-  addGrid() {
+  addGrid({ offsetWidth: width, offsetHeight: height }) {
+    console.log(width, height);
     for (let i = 1; i < Config.gridSpacing; i++) {
       // Horizontal lines
       const xLine = document.createElement("div");
       xLine.classList.add("xLine");
-      const xValue = (i * this.width) / Config.gridSpacing;
+      const xValue = (i * width) / Config.gridSpacing;
       xLine.style.left = xValue;
       elements.scene.appendChild(xLine);
       this.grid.x.push(xValue);
@@ -51,7 +50,7 @@ class App {
       // Vertical lines
       const yLine = document.createElement("div");
       yLine.classList.add("yLine");
-      const yValue = (i * this.height) / Config.gridSpacing;
+      const yValue = (i * height) / Config.gridSpacing;
       yLine.style.top = yValue;
       this.grid.y.push(yValue);
       elements.scene.appendChild(yLine);
@@ -59,7 +58,7 @@ class App {
   }
 
   // Check closest intersection
-  snapToGrid({ clientX, clientY, target }) {
+  snapToGrid({ clientX, clientY }) {
     const x = clientX - elements.scene.offsetLeft;
     const y = clientY - elements.scene.offsetTop;
     const left = this.grid.x.reduce((prev, curr) =>
@@ -71,29 +70,42 @@ class App {
     return { left, top };
   }
 
-  // Update color
-  updateColor({ target: { value } }) {
-    this.color = value;
+  // Gets color
+  getColor({ target: value }) {
+    return value;
   }
 
   // Clear scene
-  clearScene() {
+  clear() {
     elements.scene.innerHTML = "";
     this.objects = [];
-    this.addGrid();
+    this.addGrid(elements.scene);
+  }
+
+  // Clear grid only
+  clearGrid() {
+    const xLines = Array.from(document.querySelectorAll(".xLine"));
+    const yLines = Array.from(document.querySelectorAll(".yLine"));
+    const lines = [...xLines, ...yLines];
+    lines.forEach((line) => line.parentNode.removeChild(line));
   }
 
   // Add eventlisteners
   attachListeners() {
     elements.scene.addEventListener("click", this.addElement.bind(this));
-    elements.color.addEventListener("change", this.updateColor);
-    elements.clear.addEventListener("click", this.clearScene.bind(this));
+    elements.color.addEventListener("change", this.getColor);
+    elements.clear.addEventListener("click", this.clear.bind(this));
+    window.addEventListener("resize", () => {
+      this.clearGrid();
+      this.grid = { x: [], y: [] };
+      this.addGrid(elements.scene);
+    });
   }
 
   init() {
     this.toolPicker();
     this.attachListeners();
-    this.addGrid();
+    this.addGrid(elements.scene);
   }
 }
 
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 export default {
   gridSpacing: 25,
+  defaultColor: "#ededed",
 };
